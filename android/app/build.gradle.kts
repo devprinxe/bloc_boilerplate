b6823558b84dbed7108df985d81a4dbad0cf476f
Prince Mahmud <46414838+devprinxe@users.noreply.github.com>
2026-03-08 10:11:55 +0600

    Update application package name and bundle identifiers to `com.fintech.app`.
    - Update Android package name in `MainActivity.kt`, `build.gradle.kts` namespace, and `applicationId`.
    - Update iOS `PRODUCT_BUNDLE_IDENTIFIER` for app and test targets in `project.pbxproj`.
    - Add `change_app_package_name` dependency to `pubspec.yaml`.
    - Remove `pubspec.lock`.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.cartup"
+    namespace = "com.fintech.app"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = "29.0.14033849"
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.cartup"
+        applicationId = "com.fintech.app"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
--- android/app/src/main/kotlin/com/fintech/app/MainActivity.kt
+++ android/app/src/main/kotlin/com/fintech/app/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.cartup
+package com.fintech.app
 
 import io.flutter.embedding.android.FlutterActivity
 
